a1c4979cb2da5268297814a22578794e6a2712ec
André Gonçalves <andreseko@gmail.com>
2012-12-23 16:16:06 -0200

    Adicionando arquivos no repositório

---
+++ application/config/pagseguro.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['pagseguroAccount'] = 'email@dominio.com.br'; // email de acesso ao painel do pagseguro.
+$config['pagseguroToken'] = 'xxxxxxxxxx'; // token do pagseguro
---
+++ application/controllers/exemplo.php
@@ -0,0 +1,20 @@
+<?php if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
+
+class Ofertas extends CI_Controller {
+	private $pgAccount;
+  private $pgToken;
+	
+	/**
+	 * Construtor da classe
+	 */
+	public function __construct() {
+		parent::__construct ();
+		$this->load->config('pagseguro');
+		$this->load->library('PagSeguroLibrary');
+		
+		$this->pgAccount = $this->config->item ( 'pagseguroAccount' );
+		$this->pgToken = $this->config->item ( 'pagseguroToken' );
+	}
+}
+/* End of file exemplo.php */
+/* Location: ./application/controllers/exemplo.php */
